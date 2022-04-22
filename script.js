d74df40e8166974ca41b98c596cbcb796dc1942e
Aratrik Basak <75318952+Aratrik-02@users.noreply.github.com>
2022-04-22 14:51:39 +0530

    Randomized background color added

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ fetch('https://icanhazdadjoke.com/slack')
         let jokElem = document.getElementById('jok-el');
         jokElem.innerText = joke;
     })
+function random_bg_color() {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";  
+        document.body.style.background = bgColor;
+        } 
+random_bg_color();
